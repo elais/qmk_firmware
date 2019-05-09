13b2dcd33b6faca0975328399cc46a67bb5a5545
JACKSON, ELAIS <elais.jackson2@bbva.com>
2019-05-09 17:01:41 -0500

    Add minus button back to symbols layout

--- keyboards/ergodox_ez/keymaps/taco/keymap.c
+++ keyboards/ergodox_ez/keymaps/taco/keymap.c
@@ -16,23 +16,23 @@ enum {
 const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
 [BASE] = LAYOUT_ergodox( // layer 0 : default
   // left hand
-  LT(NAV,KC_ESC),  KC_1,    KC_2,       KC_3,     KC_4,     KC_5,  KC_DELETE,
-  KC_TAB,          KC_Q,    KC_W,       KC_E,     KC_R,     KC_T,  OSL(TOP),
-  LGUI(KC_DELETE), KC_A,    KC_S,       KC_D,     KC_F,     KC_G,
-  KC_LSPO,         KC_Z,    KC_X,       KC_C,     KC_V,     KC_B,  SGUI(KC_ESC),
-  MOD_HYPR,        KC_LGUI, MOD_MEH,    KC_LALT,  KC_LCTRL,   
+  LT(NAV,KC_ESC),    KC_1,    KC_2,       KC_3,     KC_4,     KC_5,  KC_DELETE,
+  KC_TAB,            KC_Q,    KC_W,       KC_E,     KC_R,     KC_T,  OSL(TOP),
+  GUI_T(KC_DELETE),  KC_A,    KC_S,       KC_D,     KC_F,     KC_G,
+  KC_LSPO,           KC_Z,    KC_X,       KC_C,     KC_V,     KC_B,  SGUI_T(KC_ESC),
+  MOD_HYPR,          KC_LGUI, MOD_MEH,    KC_LALT,  KC_LCTRL,   
                                         C(KC_LALT),          RGB_M_K,
                                                              MO(NAV),
-                             LT(SYMB,KC_SPC),    KC_DELETE, KC_LSFT,
+                             S(KC_ESC),    KC_DELETE, KC_LSFT,
   // right hand
   KC_BSPC,         KC_6,    KC_7,    KC_8,     KC_9,     KC_0,     KC_BSLS,
   CTL_T(KC_SCLN),  KC_Y,    KC_U,    KC_I,     KC_O,     KC_P,     KC_BSPC,
-                   KC_H,    KC_J,    KC_K,     KC_L,     KC_SCLN,  TD(CT_QUOT),
+                   KC_H,    KC_J,    KC_K,     KC_L,     KC_SCLN,  KC_ENT,
   ALT_T(KC_INS),   KC_N,    KC_M,    KC_COMM,  KC_DOT,   KC_SLSH,  KC_RSPC,
                             MO(NAV), KC_NO,    KC_RALT,  KC_RGUI,  C(KC_ENT),
   RGB_TOG,         KC_RCTRL,
   MO(NAV),
-  KC_RSFT,         KC_ESC,    LT(SYMB,KC_ENT)
+  KC_RSFT,         KC_ENT,    LT(SYMB,KC_SPC)
 ),
 [SYMB] = LAYOUT_ergodox(
        // left hand
@@ -46,10 +46,10 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
                                                KC_TRNS,  KC_TRNS,  KC_TRNS,
        // right hand
        KC_TRNS,  KC_TRNS,  KC_TRNS,  KC_TRNS,  KC_TRNS,  KC_TRNS,  KC_TRNS,
-  LSFT(KC_DOT),  KC_UP,    KC_7,     KC_8,     KC_9,     KC_ASTR,  KC_TRNS,
-                 KC_DOWN,  KC_4,     KC_5,     KC_6,     KC_PLUS,  KC_ENT,
-       KC_RBRC,  KC_AMPR,  KC_1,     KC_2,     KC_3,     KC_EQUAL, KC_ENT,
-                           KC_0,     KC_DOT,   KC_DOT,   KC_BSLS,  KC_TRNS,
+  LSFT(KC_DOT),  KC_UP,    KC_7,     KC_8,     KC_9,     KC_PPLS,  KC_TRNS,
+                 KC_DOWN,  KC_4,     KC_5,     KC_6,     KC_MINUS, TD(CT_QUOT),
+       KC_RBRC,  KC_AMPR,  KC_1,     KC_2,     KC_3,     KC_PAST,  KC_PEQL,
+                           KC_0,     KC_DOT,   KC_BSLS,  KC_PSLS,  KC_TRNS,
        KC_TRNS,  KC_TRNS,
        KC_TRNS,
        KC_TRNS,  KC_TRNS,  KC_TRNS
